327bbf3be42aa4d1862e5cf3a6a2aa94307318c6
puan0601 <anton.pugach@gmail.com>
2017-02-16 16:46:03 -0800

    working click to reveal form function

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,8 @@
 console.log("app.js is loaded!");
+
+$(document).ready(function(){
+
+        $("#schedulerbuttons").on('click', 'button', function() {
+            $("#entryform").toggleClass('hidden');
+        });
+    });
