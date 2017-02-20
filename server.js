ae4c4ee2b130182a050e04ab0c35129490e5694f
puan0601 <anton.pugach@gmail.com>
2017-02-20 13:52:22 -0800

    user put works fully

--- server.js
+++ server.js
@@ -48,8 +48,8 @@ var express = require("express"),
   // routes for users
   app.get("/api/users", controllers.users.index);
   // app.post("/api/users", controllers.user.create);
-  app.put("/api/users", controllers.users.update);
-  // app.delete("/api/users", controllers.user.destroy);
+  app.put("/api/users/:id", controllers.users.update);
+  // app.delete("/api/users/:id", controllers.user.destroy);
 
 
   // routes for timeSlots
