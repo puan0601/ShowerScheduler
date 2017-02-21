8be1b7046906ae7cb30dd39ec79e43a318903e45
cnwilliams <hello.christenwilliams@gmail.com>
2017-02-21 11:26:09 -0800

    adds final touches

--- server.js
+++ server.js
@@ -8,8 +8,6 @@ var express = require("express"),
 
   app.use(express.static("public"));
 
-
-
   app.get("/", function (req, res) {
     res.sendFile(__dirname + "/views/index.html");
   });
@@ -21,21 +19,16 @@ var express = require("express"),
       endpoints: [
 
         {method: "GET", path: "/api", description: "Describes all available endpoints"},
-      // users below
+        // Endpoints for users
         {method: "GET", path: "/api/users", description: "Get a list of users"},
         {method: "POST", path: "/api/users", description: "Create a new user"},
-
         {method: "GET", path: "/api/users/:id", description: "Get info on a specific user by id"},
         {method: "PUT", path: "/api/users/:id", description: "Update info on a specific user by id"},
         {method: "DELETE", path: "/api/users/:id", description: "Delete a specific user by id"},
-
-      // time slots below  -- GET, PUT, and DELETE functionality for Admin when customizing the schedule
+        // Endpoints for Time Slots
         {method: "GET", path: "/api/timeslots", description: "Get a list of timeSlots"},
-
         {method: "GET", path: "/api/timeslots/:id", description: "Get info on a specific user by id"},
         {method: "PUT", path: "/api/timeslots/:id", description: "Update info on a specific user by id"},
-        {method: "DELETE", path: "/api/timeslots/:id", description: "Delete a specific user by id"}
-
       ]
     });
   });
@@ -51,14 +44,9 @@ var express = require("express"),
   app.get("/api/users/:id", controllers.users.show);
   app.put("/api/users/:id", controllers.users.update);
 
-  // app.delete("/api/users/:id", controllers.user.destroy);
-
-
   // routes for timeSlots
   app.get("/api/timeSlots", controllers.timeSlots.index);
-  // app.post("/api/timeSlots", controllers.timeSlots.create);
   app.put("/api/timeSlots/:id", controllers.timeSlots.update);
-  // app.delete("/api/timeSlots", controllers.timeSlots.destroy);
   app.get("/api/timeSlots/:id", controllers.timeSlots.show);
 
 
