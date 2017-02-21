a5629e226323c57e030542e91d41f2d78c456a69
puan0601 <anton.pugach@gmail.com>
2017-02-21 09:47:55 -0800

    seed file works with user and timeslot now

--- seed.js
+++ seed.js
@@ -36,43 +36,43 @@ var userList = [
 var timeSlotList = [
   {
     time: '6:00am - 7:00am',
-    user: "58abf495bce6756f6755f9ca"
+    user: ""
   },
   {
     time: '7:00am - 8:00am',
-    user: "58abf495bce6756f6755f9ca"
+    user: ""
   },
     {
     time: '8:00am - 9:00am',
-    user: "58abf495bce6756f6755f9ca"
+    user: ""
   },
     {
     time: '6:00pm - 7:00pm',
-    user: "58abf495bce6756f6755f9ca"
+    user: ""
   },
   {
     time: '7:00pm - 8:00pm',
-    user: "58abf495bce6756f6755f9ca"
+    user: ""
   },
   {
     time: '8:00pm - 9:00pm',
-    user: "58abf495bce6756f6755f9ca"
+    user: ""
   }
 ];
 
 
-db.TimeSlot.remove({}, function(err, timeSlots){
-
-  db.TimeSlot.create(timeSlotList, function(err, timeSlots){
-    if (err) { return console.log("Error: " + err ); }
-  });
-  console.log(timeSlots);
-});
-
 db.User.remove({}, function(err, users) {
-
+  console.log(1);
   db.User.create(userList, function(err, users){
     if (err) { return console.log("Error: " + err ); }
-    process.exit();
+    console.log(3);
+    db.TimeSlot.remove({}, function(err, timeSlots){
+      console.log(0);
+      db.TimeSlot.create(timeSlotList, function(err, timeSlots){
+        if (err) { return console.log("Error: " + err ); }
+        console.log(2);
+        process.exit();
+      });
+    });
   });
 });
